0909f58f22d0f67142c742735d4cac9bb1a33908
Aleksandr Goncharenko <Aleks13vkok@gmail.com>
2020-10-22 22:54:42 +0300

    first commit

--- app/js/prj_load.js
+++
@@ -1,8 +0,0 @@
-var options = {
-    loadingHtml: '<div class="loader"><img src="img/images/loading.gif" alt="Loading"></div>',
-}
-
-
-$(document).ready(function() {
-    $('.jscroll').jscroll(options);
-})
